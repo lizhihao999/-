ec5a293fed3ea8c53fbf5f8733aebe49382869da
lzh <2814450867@qq.com>
2024-06-04 02:44:17 +0800

    主支提交qqqqqq

--- web/src/test/java/cn/wolfcode/web/generator/DeleFileTest.java
+++ web/src/test/java/cn/wolfcode/web/generator/DeleFileTest.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 
 public class DeleFileTest {
     public static void main(String[] args) throws IOException {
-        System.out.println("9999");
+        System.out.println("11111111111111");
 
     }
 
