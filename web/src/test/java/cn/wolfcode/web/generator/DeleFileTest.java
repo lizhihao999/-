055017f731cccfea51a50a57cacdf64dcbcafa5e
lzh <2814450867@qq.com>
2024-06-04 02:03:00 +0800

    主支提交t

--- web/src/test/java/cn/wolfcode/web/generator/DeleFileTest.java
+++ web/src/test/java/cn/wolfcode/web/generator/DeleFileTest.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 
 public class DeleFileTest {
     public static void main(String[] args) throws IOException {
-        System.out.println("主支");
+        System.out.println("主支xxx");
 
     }
 
