681a4ff5611f508b28aea013e6b36d6e38b47ce2
lzh <2814450867@qq.com>
2024-06-04 03:36:41 +0800

    远程分支测试

--- web/src/test/java/cn/wolfcode/web/generator/DeleFileTest.java
+++ web/src/test/java/cn/wolfcode/web/generator/DeleFileTest.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 
 public class DeleFileTest {
     public static void main(String[] args) throws IOException {
-        System.out.println("11111111111111");
+        System.out.println("pushlslls");
 
     }
 
