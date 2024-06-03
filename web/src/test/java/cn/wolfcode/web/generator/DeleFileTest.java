1bf2096231bb87eca8636f916502b863429c3351
lzh <2814450867@qq.com>
2024-06-04 02:32:55 +0800

    主支提交66666

--- web/src/test/java/cn/wolfcode/web/generator/DeleFileTest.java
+++ web/src/test/java/cn/wolfcode/web/generator/DeleFileTest.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 
 public class DeleFileTest {
     public static void main(String[] args) throws IOException {
-        System.out.println("444444");
+        System.out.println("666666");
 
     }
 
